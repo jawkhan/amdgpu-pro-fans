fd144a2544bb04b68ffe452adb4a7c68ae881928
Jawaad Khan <Jaw_khan@hotmail.co.uk>
2017-12-10 15:03:29 +0000

    Update amdgpu-pro-fans.sh

--- amdgpu-pro-fans.sh
+++ amdgpu-pro-fans.sh
@@ -82,24 +82,25 @@ set_adapter_fan_speeds ()
     for CurrentCard in  /sys/class/drm/card?/ -o /sys/class/drm/card??/ ; do
          for CurrentMonitor in "$CurrentCard"device/hwmon/hwmon?/ -o "$CurrentCard"device/hwmon/hwmon??/ ; do
             if [$cardcount == $adapter]; then 
-              cd $CurrentMonitor # &>/dev/null
-              workingdir="`pwd`"
-              fanmax=$(head -1 "$workingdir"/pwm1_max)
-              if [ $fanmax -gt 0 ] ; then    
-                  speed=$(( fanmax * fanpercent ))
-                  speed=$(( speed / 100 ))
-                  sudo chown $USER "$workingdir"/pwm1_enable
-                  sudo chown $USER "$workingdir"/pwm1
-                  sudo echo -n "1" >> $workingdir/pwm1_enable # &>/dev/null
-                  sudo echo -n "$speed" >> $workingdir/pwm1 # &>/dev/null
-                  speedresults=$(head -1 "$workingdir"/pwm1)
-                  if [ $(( speedresults - speed )) -gt 6 ] ; then
-                       echo "Error Setting Speed For Card$cardcount!"
+                  cd $CurrentMonitor # &>/dev/null
+                  workingdir="`pwd`"
+                  fanmax=$(head -1 "$workingdir"/pwm1_max)
+                  if [ $fanmax -gt 0 ] ; then    
+                      speed=$(( fanmax * fanpercent ))
+                      speed=$(( speed / 100 ))
+                      sudo chown $USER "$workingdir"/pwm1_enable
+                      sudo chown $USER "$workingdir"/pwm1
+                      sudo echo -n "1" >> $workingdir/pwm1_enable # &>/dev/null
+                      sudo echo -n "$speed" >> $workingdir/pwm1 # &>/dev/null
+                      speedresults=$(head -1 "$workingdir"/pwm1)
+                      if [ $(( speedresults - speed )) -gt 6 ] ; then
+                           echo "Error Setting Speed For Card$cardcount!"
+                      else
+                           echo "Card$cardcount Speed Set To $fanpercent %"
+                      fi
                   else
-                       echo "Card$cardcount Speed Set To $fanpercent %"
+                      echo "Error: Unable To Determine Maximum Fan Speed For Card$cardcount!"
                   fi
-              else
-                  echo "Error: Unable To Determine Maximum Fan Speed For Card$cardcount!"
               fi
          done
          cardcount="$(($cardcount + 1))"
